d7bfb5a4056e20a6db5898b4e296dfd08686ee9a
masud4657 <masudpfrnt@gmail.com>
2024-02-14 14:44:29 +0600

    added the playground section

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,9 +1,26 @@
-function play() {
+// function play() {
 
 
-    const homeSection = document.getElementById('home-screen');
-    homeSection.classList.add('hidden')
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden')
 
-    const playgroundSection = document.getElementById('play-ground');
-    playgroundSection.classList.remove('hidden')
+//     const playgroundSection = document.getElementById('play-ground');
+//     playgroundSection.classList.remove('hidden')
+// }
+
+
+function continueGame(){
+    const alphabet = getRandomAlphabets()
+    console.log('your random alphabet:', alphabet)
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet
+
+    setBackgroundColor(alphabet)
+}
+
+function play(){
+    hideElementById('home-screen');
+    removeElementById('play-ground');
+    continueGame();
 }
---
+++ scripts/utility.js
@@ -0,0 +1,30 @@
+function hideElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden')
+}
+
+function removeElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden')
+}
+
+
+function setBackgroundColor(elementId){
+    const element = document.getElementById(elementId)
+    element.classList.add('bg-amber-500')
+}
+
+
+function getRandomAlphabets(){
+    const alphabetSting = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetSting.split('');
+    // console.log(alphabets)
+
+    const randomNumbers = Math.random()*25;
+    const index = Math.round(randomNumbers);
+    
+
+    const alphabet = alphabets[index];
+    // console.log(index, alphabet)
+    return alphabet
+}
