24e1ee7ecd3709300869d73ae22ad1208a94403e
masud4657 <masudpfrnt@gmail.com>
2024-02-14 11:07:19 +0600

    addede the hiddden home screen and display the playgroundsection

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,9 @@
+function play() {
+
+
+    const homeSection = document.getElementById('home-screen');
+    homeSection.classList.add('hidden')
+
+    const playgroundSection = document.getElementById('play-ground');
+    playgroundSection.classList.remove('hidden')
+}
